410fa275f997a1a898105216945dfbf86beaa536
jessica kilby <jessicakilby@gmail.com>
2016-11-15 18:56:55 -0600

    finishing day 1

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,6 +9,7 @@ app.controller("NavCtrl", function($scope){
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
 	$scope.showListView = true;
+	$scope.newTask = {};
 	$scope.items = [
 		{
 			id: 0,
@@ -30,13 +31,28 @@ app.controller("TodoCtrl", function($scope){
 		}
 	];
 
+
 	$scope.allItems = function(){
 		console.log("you clicked all items");
 		$scope.showListView = true;
-	}
+	};
 	$scope.newItem = function(){
 		console.log("you clicked new item");
 		$scope.showListView = false;
-	}
+	};
+	$scope.addNewItem = function(){
+		$scope.newTask.isCompleted = false;
+		$scope.newTask.id = $scope.items.length;
+		console.log("newTask in function", $scope.newTask);
+		$scope.items.push($scope.newTask);
+		$scope.newTask = {};
+		$scope.showListView = true;
 
-})
+
+	};
+
+
+
+
+
+});
