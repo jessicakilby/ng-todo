8021ade56dfed9df23a6fe9304e685681aaff551
jessica kilby <jessicakilby@gmail.com>
2016-11-19 11:38:06 -0600

    got rid of showListView and used routes

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -1,5 +1,21 @@
 "use strict";
 
-app.controller("ItemListCtrl", function($scope){
-	$scope.message = "ItemListCtrl";
+app.controller("ItemListCtrl", function($scope, ItemFactory){
+	
+	$scope.items = [];
+
+	let getItems = function(){
+		ItemFactory.getItemList().then(function(fbItems){
+			console.log("items from controller", fbItems);
+			$scope.items = fbItems;
+		});
+	};
+	getItems();	
+
+	$scope.deleteItem = function(itemId){
+		console.log("delete item", itemId);
+		ItemFactory.deleteItem(itemId).then(function(response){
+			getItems();
+		});
+	};
 });
--- javascripts/controllers/ItemViewCtrl.js
+++ javascripts/controllers/ItemViewCtrl.js
@@ -1,5 +1,17 @@
 "use strict";
 
-app.controller("ItemViewCtrl", function($scope){
-	$scope.message = "ItemViewCtrl";
+app.controller("ItemViewCtrl", function($scope, $location, ItemFactory){
+	
+	$scope.newTask = {};
+
+	$scope.addNewItem = function(){
+		$scope.newTask.isCompleted = false;
+		ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+			$location.url("/items/list");
+			$scope.newTask = {};
+		});
+	};
+
 });
+
+//$location
