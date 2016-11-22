e2c425abf06fc4fa6bf006de9b52438ac0b42a7e
jessica kilby <jessicakilby@gmail.com>
2016-11-21 18:53:19 -0600

    checkbox working

--- javascripts/factories/itemFactory.js
+++ javascripts/factories/itemFactory.js
@@ -47,7 +47,42 @@ app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG){
 		});
 	};
 
-	return {getItemList:getItemList, postNewItem:postNewItem, deleteItem:deleteItem};
+	var getSingleItem = function(itemId){
+		return $q((resolve, reject)=>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/items/${itemId}.json`)
+			.success(function(getSingleResponse){
+				resolve(getSingleResponse);
+			}).error(function(postError){
+				reject(postError);
+			});
+		});
+	};
+
+	var editItem = function(editItem){
+		console.log("factory edit", editItem);
+		return $q((resolve, reject)=>{
+			$http.put(`${FIREBASE_CONFIG.databaseURL}/items/${editItem.id}.json`, JSON.stringify({
+				assignedTo: editItem.assignedTo,
+				isCompleted: editItem.isCompleted,
+				task: editItem.task
+				})
+			)
+			.success(function(editResponse){
+				resolve(editResponse);
+			})
+			.error(function(editError){
+				reject(editError);
+			});
+		});
+	};
+
+	return {
+		getItemList:getItemList, 
+		postNewItem:postNewItem, 
+		deleteItem:deleteItem, 
+		getSingleItem:getSingleItem,
+		editItem:editItem
+	};
 });
 
 	// 	return new Promise((resolve, reject)=>{
